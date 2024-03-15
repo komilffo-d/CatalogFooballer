de3ad0f94b4b0b0754c319f782bfe701c03f7d50
dev-Macintosh <ivan.200512340@gmail.com>
2024-03-15 22:03:48 +0300

    fix SignalR subcriber modification HTML code

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,7 +10,9 @@ hub.on("PlayerAdd", function (playerId, player) {
     let keyName = ["surname","name", "sex", "dateBirthday","command", "country",];
     const tableBody = $(".table >  tbody").get(0);
     let tr = document.createElement("tr");
-    tr.setAttribute("data-player", playerId.toString())
+    tr.setAttribute("data-player", playerId.toString());
+    tr.classList.add("table-row");
+
     keyName.forEach((k) => {
         let td = document.createElement("td");
         switch (k) {
@@ -27,12 +29,15 @@ hub.on("PlayerAdd", function (playerId, player) {
     });
 
     let td = document.createElement("td");
-    let a = document.createElement("a");
-    a.textContent = "Edit";
-    a.href = `/Player/Edit/${playerId}`;
-    td.appendChild(a);
+    td.innerHTML = `
+        <div class="table-row__edit">
+            <div class="table-row__edit__image">
+                <img src="/images/vectors/pen_edit.svg" width="30">
+            </div>
+            <a class="table-row__edit__link" href="/Player/Edit/${playerId}">Редактировать игрока</a>
+        </div>
+    `;
     tr.appendChild(td);
-
 
     tableBody.appendChild(tr);
 
@@ -43,7 +48,7 @@ hub.on("PlayerModify", function (playerId, player) {
 
     let keyName = ["surname", "name", "sex", "dateBirthday","command", "country", ];
     const tableBody = $(".table >  tbody").get(0);
-    let tr = $(tableBody).find(`[data-player=${playerId}]`).empty().get(0);
+    let tr = $(tableBody).find(`.table-row[data-player=${playerId}]`).empty().get(0);
 
     keyName.forEach((k) => {
         let td = document.createElement("td");
@@ -61,10 +66,14 @@ hub.on("PlayerModify", function (playerId, player) {
     });
 
     let td = document.createElement("td");
-    let a = document.createElement("a");
-    a.textContent = "Edit";
-    a.href = `/Player/Edit/${playerId}`;
-    td.appendChild(a);
+    td.innerHTML = `
+        <div class="table-row__edit">
+            <div class="table-row__edit__image">
+                <img src="/images/vectors/pen_edit.svg" width="30">
+            </div>
+            <a class="table-row__edit__link" href="/Player/Edit/${playerId}">Редактировать игрока</a>
+        </div>
+    `;
     tr.appendChild(td);
 
 
